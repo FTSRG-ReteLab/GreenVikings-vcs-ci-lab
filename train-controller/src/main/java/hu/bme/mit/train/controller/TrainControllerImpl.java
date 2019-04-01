ada1c49180a1e61ef38c67e2c926e946c057084d
kelemendomonkos <32821607+kelemendomonkos@users.noreply.github.com>
2019-04-01 06:27:20 -0700

    Update TrainControllerImpl.java

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -3,6 +3,11 @@ package hu.bme.mit.train.controller;
 import hu.bme.mit.train.interfaces.TrainController;
 
 public class TrainControllerImpl implements TrainController {
+	
+	public TrainControllerImpl(){
+		Timer timer = new Timer();
+		timer.start();
+	}
 
 	private int step = 0;
 	private int referenceSpeed = 0;
@@ -44,6 +49,21 @@ public class TrainControllerImpl implements TrainController {
 	@Override
 	public void setJoystickPosition(int joystickPosition) {
 		this.step = joystickPosition;		
+	}
+	
+	public class Timer extends Thread implements Runnable{
+
+		@Override
+		public void run() {
+			while(true) {
+				followSpeed();
+				try {
+					Thread.sleep(60);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 
 }
