b2321ad49ea097f6717e96a30a20e792d8bbb399
kelemendomonkos <32821607+kelemendomonkos@users.noreply.github.com>
2019-04-01 07:11:58 -0700

    Fixed code smell

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -3,7 +3,6 @@ package hu.bme.mit.train.controller;
 import hu.bme.mit.train.interfaces.TrainController;
 
 import java.util.Timer;
-import java.util.TimerTask;
 
 public class TrainControllerImpl implements TrainController {
 
