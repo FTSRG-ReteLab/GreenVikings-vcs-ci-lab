0b7f9d134a708f9ff779bc407d8bdc65dc2b5407
Nasztor Mark <nasztor.mark96@gmail.com>
2019-04-01 08:03:27 -0700

    Test without mockito import

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -1,13 +1,23 @@
 package hu.bme.mit.train.sensor;
 
+import hu.bme.mit.train.controller.TrainControllerImpl;
+import hu.bme.mit.train.interfaces.TrainController;
+import hu.bme.mit.train.interfaces.TrainUser;
+import hu.bme.mit.train.user.TrainUserImpl;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.mockito.Mockito.*;
+
 
 public class TrainSensorTest {
 
-    TrainUser user;
+    TrainController controller = new TrainControllerImpl();
+
+    @Mock
+    private TrainUserImpl user = new TrainUserImpl(controller);
+
     private int testval = 5;
     @Test
     public void SelfTest() {
@@ -18,7 +28,7 @@ public class TrainSensorTest {
     public void setAlarmManualTest(){
         user = mock(TrainUserImpl.class);
         user.setAlarmState(true);
-        Assert.AssertTrue(user.getAlarmState);
+        Assert.assertTrue(user.getAlarmState());
     }
 }
 
